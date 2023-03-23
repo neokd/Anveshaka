7daad847a9e6d4174cf15ae177937c6232b889fe
Kuldeep <71772185+neokd@users.noreply.github.com>
2023-03-23 06:13:18 +0000

    Updated Scraper

--- backend/scrape.py
+++ backend/scrape.py
@@ -2,10 +2,15 @@ from newspaper import Article,ArticleException
 import requests
 import bs4
 import json
+import nltk
+
+try:
+    nltk.data.find('tokenizers/punkt.zip')
+except Exception:
+    nltk.download('punkt')
 
 # Sample URL
 #https://news.google.com/articles/CBMic2h0dHBzOi8vd3d3LmxpdmVtaW50LmNvbS9icmFuZC1zdG9yaWVzL2hvdy1nZWVrc2ZvcmdlZWtzLWlzLW1ha2luZy1wcm9ncmFtbWluZy1lYXNpZXItd29ybGR3aWRlLTExNjQ5MDc5ODQ3MDQ4Lmh0bWzSAXdodHRwczovL3d3dy5saXZlbWludC5jb20vYnJhbmQtc3Rvcmllcy9ob3ctZ2Vla3Nmb3JnZWVrcy1pcy1tYWtpbmctcHJvZ3JhbW1pbmctZWFzaWVyLXdvcmxkd2lkZS9hbXAtMTE2NDkwNzk4NDcwNDguaHRtbA?hl=en-IN&gl=IN&ceid=IN%3Aen
-
 
 def scrape_google_news(text):
     url = f'https://news.google.com/search?for={text}'
@@ -21,34 +26,55 @@ def scrape_google_news(text):
             scrape_links.append('https://news.google.com'+a_tag['href'][1:])  
     return scrape_links
 
+def scrape_google(text):
+    url = 'https://google.com/search?q=' + text
+    request_result=requests.get( url )
+    soup = bs4.BeautifulSoup(request_result.text,"html.parser")
+
+    scrape_links = []
+    scrape_links.append({"searchKey":text.capitalize()})
+    for a in soup.find_all('a',href=True):
+        for i in a.find_all('h3'):
+            scrape_links.append({
+                "title":i.getText(),
+                "link": a['href'].split('/url?q=',1)[1].split('&',1)[0]
+            },)
+    return scrape_links
+
+def write_json(links):
+    i=0
+    json_write = []
+    for link in links:
+        article = Article(link, language="en")
+        try:
+            i+=1
+            article.download()
+            article.parse()
+            article.nlp()
+        except ArticleException:
+            pass
+        print('-----------------------------------------> '+str(i))
+        json_data = {
+            "url":link,
+            "title": article.title,
+            "description": article.text,
+            "summary":article.summary,
+            "keywords":article.keywords
+        }
+        json_write.append(json_data)
+    with open('temp.json','w') as json_file:
+        json.dump(json_write,json_file,indent=4, separators=(',', ': '))
+
+def extract_json():
+    json_file = open('temp.json')
+    data = json.load(json_file)
+    for i in data:
+        print(i['summary'])
+
+if __name__ == '__main__':
+    links = (scrape_google_news('python'))
+    write_json(links)
+    extract_json()
+    
 
 
-links = scrape_google_news('earthquake')
-i=0
-json_write = []
-for link in links:
-    article = Article(link, language="en")
-    try:
-        i+=1
-        article.download()
-        article.parse()
-        article.nlp()
-    except ArticleException:
-        pass
-    print('-----------------------------------------> '+str(i))
-    json_data = {
-        "url":link,
-        "title": article.title,
-        "description": article.text,
-        "summary":article.summary,
-        "keywords":article.keywords
-    }
-    json_write.append(json_data)
-with open('backend/temp.json','w') as json_file:
-    json.dump(json_write,json_file,indent=4, separators=(',', ': '))
-   
-# import json
-# json_file = open('backend/temp.json')
-# data = json.load(json_file)
-# for i in data:
-#     print(i['summary'])
